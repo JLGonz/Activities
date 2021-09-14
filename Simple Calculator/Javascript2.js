41666799b2dc9386783a93fa61824a1cc0473024
Joseh Laurence Gonza <chriseph21@gmail.com>
2021-09-14 11:02:56 +0800

    Auto stash before merge of "main" and "Activities/main"

--- Simple Calculator/Javascript2.js
+++ Simple Calculator/Javascript2.js
@@ -1,4 +1,8 @@
 let first = document.getElementById("input1");
 let second = document.getElementById("input2");
 
+<<<<<<< Updated upstream
 button.addEventListener("click")
+=======
+button.addEventListener("click")
+>>>>>>> Stashed changes
